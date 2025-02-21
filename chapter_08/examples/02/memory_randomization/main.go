51abf3fbe06c2e233ea2e998f8dfc24c16420c98
mikail-eliyah-00 <eliyah.mikail@gmail.com>
2025-02-20 23:04:58 -0800

    .

--- chapter_08/examples/02/memory_randomization/main.go
+++ chapter_08/examples/02/memory_randomization/main.go
@@ -44,3 +44,18 @@ func main() {
 	// The original data should not be accessible anymore
 	fmt.Println("Reference deleted. Original content should no longer be retrievable.")
 }
+
+/*
+Sample run:
+% go run main.go
+Original string: you should be able to see me
+Hex representation: 796f752073686f756c642062652061626c6520746f20736565206d65
+Before deletion, memory contains (string): you should be able to see me
+After randomization, memory contains (string): !
+                                                ??
+                                                  ?_r??2???
+                                                           FO?
+                                                              ?`??+??J
+After randomization, memory contains (in hex): 210c8c940bf05f72f3fa32f8bdf60c464fa20bdb6082df1f2bfbd44a
+Reference deleted. Original content should no longer be retrievable.
+*/
